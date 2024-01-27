72ec625e68f520151b2edfadfc28c9fcd762e2a4
subhash131 <subhashnayak131@gmail.com>
2024-01-27 19:33:36 +0530

    create component: Features

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div className="h-screen w-full flex justify-center">
+      <div>
+        <p className="text-6xl leading-tight">A platform designed for growth</p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
