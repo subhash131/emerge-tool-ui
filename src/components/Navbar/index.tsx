0d338947bb2f7f830535072cef4ab9f454624ddd
subhash131 <subhashnayak131@gmail.com>
2024-01-27 12:40:04 +0530

    update component: navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,19 +11,23 @@ const items = [
 
 const Navbar = () => {
   return (
-    <div className="w-full h-20 flex items-center px-8 justify-between bg-transparent backdrop-blur-md bg--600 text-sm fixed top-0">
-      <div className="py-2 border border-[#616160] relative px-6">
-        <div className="w-1 h-1 rounded-full bg-white absolute -top-[0.1rem] -left-[0.1rem]"></div>
-        <div className="w-1 h-1 rounded-full bg-white absolute -top-[0.1rem] -right-[0.1rem]"></div>
-        <div className="w-1 h-1 rounded-full bg-white absolute -bottom-[0.1rem] -right-[0.1rem]"></div>
-        <div className="w-1 h-1 rounded-full bg-white absolute -bottom-[0.1rem] -left-[0.1rem]"></div>
+    <div className="w-full h-20 flex items-center px-8 justify-between backdrop-blur-sn bg-black text-sm fixed top-0 z-50">
+      <div className="py-2 border border-[#616160] relative px-6 bg-black ">
+        <div className="w-1 h-1 rounded-full bg-white absolute -top-[0.2rem] -left-[0.2rem]"></div>
+        <div className="w-1 h-1 rounded-full bg-white absolute -top-[0.2rem] -right-[0.2rem]"></div>
+        <div className="w-1 h-1 rounded-full bg-white absolute -bottom-[0.2rem] -right-[0.2rem]"></div>
+        <div className="w-1 h-1 rounded-full bg-white absolute -bottom-[0.2rem] -left-[0.2rem]"></div>
         Emerge Tools
       </div>
       <div className="flex gap-10 ">
         <ul className="flex gap-2 text-xs items-center ">
           {items.map((item, index) => {
             return (
-              <li className="cursor-pointer p-2" key={`nav-${index}`}>
+              <li
+                className="p-2 pointer-events-auto cursor-wait"
+                key={`nav-${index}`}
+                style={{ cursor: "pointer" }}
+              >
                 {item}
               </li>
             );
