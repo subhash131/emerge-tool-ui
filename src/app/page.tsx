744902ab7bb3caf2bbdd39d3f0392e0135880173
subhash131 <subhashnayak131@gmail.com>
2024-01-27 19:32:40 +0530

    add component: Features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-screen overflow-hidden">
       <Hero />
+      <Features />
     </main>
   );
 }
