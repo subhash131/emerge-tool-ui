86e103ea558b5d9a47eb373b57c7fc6cb6f923f1
subhash131 <subhashnayak131@gmail.com>
2024-01-27 19:34:39 +0530

    update component: add infinite scroll

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import Button from "../Button";
+import styles from "./hero.module.css";
+import { Drum, GitCompareArrows, Rocket, Scaling, Skull } from "lucide-react";
 
 const Hero = () => {
   return (
@@ -34,8 +36,8 @@ const Hero = () => {
           />
         </div>
       </div> */}
-      <div className="w-full h-full flex justify-center items-center absolute z-20">
-        <div className="w-[40rem] h-[18rem] relative flex flex-col gap-6  -mt-32 text-center">
+      <div className="w-full h-full gap-24 flex justify-center flex-col items-center absolute z-30 ">
+        <div className="w-[40rem] h-[18rem] relative flex flex-col gap-16 -mt-32 text-center">
           <div className="flex flex-col gap-4 items-center ">
             <p className="text-6xl leading-tight">
               Join the future of mobile development
@@ -45,9 +47,64 @@ const Hero = () => {
               supercharge mobile apps and the teams that build them
             </p>
           </div>
-          <Button className="px-7 shadow-gray-300 " fontSize="text-lg">
+          <Button className="px-7 pointer-events-auto" fontSize="text-lg">
             Get Started
           </Button>
+        </div>
+        <div
+          className={` overflow-hidden relative w-[calc(100px*5)]  before:bg-gradient-linear-to-r before:h-full before:w-[15%] before:z-10 before:absolute before:-right-1 before:top-0 after:absolute after:w-[15%] after:h-full after:bg-gradient-linear-to-l after:z-10 after:top-0 after:left-0 whitespace-nowrap flex items-center h-5 ${styles.slide_wrap}`}
+        >
+          <ul
+            className={`inline-block text-xs items-center ${styles.logo_slide}`}
+          >
+            <div className="flex gap-8 ml-8">
+              <li className="flex gap-1">
+                <Scaling size={16} strokeWidth={1} />
+                Size Analysis
+              </li>
+              <li className="flex gap-1">
+                <Drum size={16} strokeWidth={1} />
+                Performance Analysis
+              </li>
+              <li className="flex gap-1">
+                <GitCompareArrows size={16} strokeWidth={1} />
+                Snapshots
+              </li>
+              <li className="flex gap-1">
+                <Rocket size={16} strokeWidth={1} /> Launch Booster
+              </li>
+              <li className="flex gap-1">
+                <Skull size={16} strokeWidth={1} />
+                Reaper
+              </li>
+            </div>
+          </ul>
+          {/* Copy of same items */}
+          <ul
+            className={`inline-block  text-xs items-center ${styles.logo_slide}`}
+          >
+            <div className="flex gap-8 ml-8">
+              <li className="flex gap-1">
+                <Scaling size={16} strokeWidth={1} />
+                Size Analysis
+              </li>
+              <li className="flex gap-1">
+                <Drum size={16} strokeWidth={1} />
+                Performance Analysis
+              </li>
+              <li className="flex gap-1">
+                <GitCompareArrows size={16} strokeWidth={1} />
+                Snapshots
+              </li>
+              <li className="flex gap-1">
+                <Rocket size={16} strokeWidth={1} /> Launch Booster
+              </li>
+              <li className="flex gap-1">
+                <Skull size={16} strokeWidth={1} />
+                Reaper
+              </li>
+            </div>
+          </ul>
         </div>
       </div>
     </div>
