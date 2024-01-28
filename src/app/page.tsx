4620b0382215e22a9c3d3dcbaf49611c9736ea5c
subhash131 <subhashnayak131@gmail.com>
2024-01-28 15:11:27 +0530

    add components: testimonials, footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonial";
 
 export default function Home() {
   return (
     <main className="w-screen overflow-hidden">
       <Hero />
       <Features />
+      <Testimonials />
+      <Footer />
     </main>
   );
 }
