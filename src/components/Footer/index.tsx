21b9161bff032a1467238765f8872bf4939f93c5
subhash131 <subhashnayak131@gmail.com>
2024-01-29 02:46:15 +0530

    update component: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Footer = () => {
+  const date = new Date();
   return (
-    <div className="w-full py-16 px-10">
+    <div className="w-full h-14 px-10 text-gray-400 mt-10 text-sm">
       <hr className="border border-[#616160]" />
+      <div className="h-full w-full flex items-center justify-between">
+        <p>
+          {date.getFullYear()} &#169;{" "}
+          <a href="mailto:subhashnayak131@gmail.com">
+            subhashnayak131@gmail.com
+          </a>
+        </p>
+        <ul className="flex gap-2">
+          <li>Tech used to build this website:</li>
+          <li>Next.js,</li>
+          <li>Tailwind CSS</li>
+        </ul>
+      </div>
     </div>
   );
 };
