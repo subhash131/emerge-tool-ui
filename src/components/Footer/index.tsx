30d4cadd114e6dacb64ab8b008c3b497673622c9
subhash131 <subhashnayak131@gmail.com>
2024-01-28 15:12:48 +0530

    create component: footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full py-16 px-10">
+      <hr className="border border-[#616160]" />
+    </div>
+  );
+};
+
+export default Footer;
