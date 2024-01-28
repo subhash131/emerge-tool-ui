98b1dbcf90fb7142de3eedf0313ee0e9a42b45fe
subhash131 <subhashnayak131@gmail.com>
2024-01-28 23:19:00 +0530

    add component: card3

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Card1 from "./Card1";
+import Card2 from "./Card2";
+import Card3 from "./Card3";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="mt-32 flex gap-20 px-28">
+      <Card1 />
+      <div className="flex flex-col gap-2">
+        <Card3>Customizable slack alerting</Card3>
+        <Card3>Accurately measure performance metrics</Card3>
+        <Card3>API access to raw performance data</Card3>
+        <Card3>Run a performance comparison of any two builds</Card3>
+      </div>
+      <Card2 />
+    </div>
+  );
 };
 
 export default Features;
